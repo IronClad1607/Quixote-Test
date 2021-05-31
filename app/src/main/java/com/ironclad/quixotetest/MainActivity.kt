98ff23f8dff6762693bf2329274f0525f659031d
Ishaan Jain <ishaan160799@gmail.com>
2021-06-01 00:50:53 +0530

    layout ready

--- app/src/main/java/com/ironclad/quixotetest/MainActivity.kt
+++ app/src/main/java/com/ironclad/quixotetest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ironclad.quixotetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ironclad.quixotetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
