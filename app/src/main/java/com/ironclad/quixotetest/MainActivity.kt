3b33ffd6f47db9768b2ded72db66ff6b597f3ac5
Ishaan Jain <ishaan160799@gmail.com>
2021-06-01 12:55:01 +0530

    infinite rotation added

--- app/src/main/java/com/ironclad/quixotetest/MainActivity.kt
+++ app/src/main/java/com/ironclad/quixotetest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ironclad.quixotetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import com.ironclad.quixotetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        val rotate = RotateAnimation(
+            0f, 360f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        rotate.duration = 900
+        rotate.repeatCount = Animation.INFINITE
+
+        binding?.imageArrow?.startAnimation(rotate)
     }
 
     override fun onDestroy() {
